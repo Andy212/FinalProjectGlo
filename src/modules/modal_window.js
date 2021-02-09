c99caebaaa92411396188b364615e08091f8125e
Andy Dzemichau <andyuson24@gmail.com>
2021-02-09 11:19:13 +0200

    make closeBtn

--- src/modules/modal_window.js
+++ src/modules/modal_window.js
@@ -1,18 +1,28 @@
 const modalWindow = () => {
     const btnCallback = document.querySelectorAll('.header a'),
         callbackInner = document.querySelector('.modal-callback'),
-        callbackBack = document.querySelector('.modal-overlay');
-        // btnClose = document.querySelector('.modal-close');
+        callbackBack = document.querySelector('.modal-overlay'),
+        btnClose = document.querySelector('.modal-close');
     
+
+        const closeAll = () => {
+            callbackBack.style.display = 'none';
+            callbackInner.style.display = 'none';
+        }
+
 
         btnCallback[1].addEventListener('click', () =>{
             callbackBack.style.display = 'block';
             callbackInner.style.display = 'block';
-    })
+    });
 
-        // btnClose.addEventListener('click', () => {
-            
-        // })
+        callbackBack.addEventListener('click', () => {
+            closeAll();
+        });
+
+        btnClose.addEventListener('click', () => {
+            closeAll();       
+        });
 }
 
 export default modalWindow;
