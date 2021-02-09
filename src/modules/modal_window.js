24c38e18bf0f91c01b04311ff865ed4ebf22e743
Andy Dzemichau <andyuson24@gmail.com>
2021-02-09 22:14:20 +0200

    fix callback form

--- src/modules/modal_window.js
+++ src/modules/modal_window.js
@@ -3,7 +3,8 @@ const modalWindow = () => {
         btnQuestion = document.querySelector('.quest-section a'),
         callbackInner = document.querySelector('.modal-callback'),
         callbackBack = document.querySelector('.modal-overlay'),
-        btnClose = document.querySelector('.modal-close');
+        btnClose = document.querySelector('.modal-close'),
+        btnCallCarusel = document.querySelectorAll('.element a');
     
 
         const closeAll = () => {
@@ -14,7 +15,10 @@ const modalWindow = () => {
             callbackBack.style.display = 'block';
             callbackInner.style.display = 'block';
         }
-
+        for(let i = 0; i < btnCallCarusel.length; i++)
+        btnCallCarusel[i].addEventListener('click', () => {
+            openAll();
+        });
 
         btnCallback[1].addEventListener('click', () =>{
             openAll();
@@ -23,6 +27,7 @@ const modalWindow = () => {
         btnQuestion.addEventListener('click', () =>{
             openAll();
         });
+
 
         callbackBack.addEventListener('click', () => {
             closeAll();
