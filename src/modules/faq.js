e7f32b48a9eaac2dcc4b65edf36c2bed92476834
Andy Dzemichau <andyuson24@gmail.com>
2021-02-10 15:06:27 +0200

    add accordeon and carusel

--- src/modules/faq.js
+++ src/modules/faq.js
@@ -1,4 +1,19 @@
 const faq = () => {
+    const accElements = document.querySelectorAll('.acc');
+    const closeOpen = () => {
+
+        accElements.forEach((section) => {
+            section.addEventListener('click', (e) =>{
+                
+                accElements.forEach((section) =>{
+                    section.classList.remove('active');
+                })
+                e.target.closest('.acc').classList.add('active');
+            })
+        });
+
+    }
+    closeOpen();
 
 }
 export default faq;
--- src/modules/services.js
+++ src/modules/services.js
@@ -1,6 +1,35 @@
 const services = () => {
-    
-    
+    const items = document.querySelectorAll('.carouselSlide'),
+        btnLeft = document.querySelector('.arrow-left'),
+        btnRight = document.querySelector('.arrow-right');
+        let activeSlide = 0;
+
+        const renderSlide = () =>{
+            for(let i = 0; i < items.length; i++){
+                if(i >= activeSlide && i <= activeSlide + 2){
+                    items[i].classList.add('activeCarousel');
+                } else {
+                    items[i].classList.remove('activeCarousel');
+                }
+            }
+        } 
+
+        const prev = () => {
+            if(activeSlide - 1 >= 0){
+                activeSlide--;
+                renderSlide();
+            }
+        };
+
+        const next = () =>{
+            if(activeSlide + 3 < items.length){
+                activeSlide++; 
+                renderSlide();
+            }
+        };
+
+        btnLeft.addEventListener('click', prev);
+        btnRight.addEventListener('click', next);
 
 }
 
